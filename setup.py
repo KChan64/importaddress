81e8f3c720be983bf995a4984905e2c717358cef
kcorlidy <1220181099@qq.com>
2019-08-25 17:37:29 +0800

    beta

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with io.open(readme_path, encoding="utf-8") as read_file:
     long_description = read_file.read()
 
 setup(name="importaddress",
-      version="1.1",
+      version="1.1beta",
       description="Bitcoin addresses generator by using HD protocol",
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -22,12 +22,10 @@ setup(name="importaddress",
       packages=["importaddress"],
       package_data={"importaddress": ["data/*.md"]},
       license="Apache",
-      python_requires=">2.6, >3.3",
+      python_requires=">3.3",
+      # https://pypi.org/classifiers/
       classifiers=[
           "Programming Language :: Python",
-          "Programming Language :: Python :: 2.6",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3",
           "Programming Language :: Python :: 3.4",
           "Programming Language :: Python :: 3.5",
           "Programming Language :: Python :: 3.6",
